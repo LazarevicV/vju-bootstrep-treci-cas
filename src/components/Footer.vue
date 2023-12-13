b43c29810a5b5025b8026f51dc860221aec73851
LazarevicV <vukadinlazarevic@gmail.com>
2023-12-13 22:47:24 +0100

    dodat footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="page-footer font-small primary">
+    <div class="footer-copyright text-center py-3">
+      © 2020 Copyright:
+      <a href="https://raf.edu.rs/"> RAF</a>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+    setup() {
+      return {};
+    },
+  };
+</script>
+
+<style scoped>
+  footer {
+    background-color: darkgrey;
+  }
+</style>
